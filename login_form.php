0328c84844ea40fb0f0f591fbb28c4f9ef90e2ec
Surya Teja Achanta <suryateja.a16@iiits.in>
2018-07-12 19:31:22 +0530

    Update login_form.php

--- login_form.php
+++ login_form.php
@@ -27,7 +27,7 @@
 <title>SIGN IN</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="php SignUp,Signin Form template Responsive,surya teja achanta, virusurya,virusattack" />
+<meta name="keywords" content="php SignUp-Signin Form template Responsive,surya teja achanta, virusurya,virusattack" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- Custom Theme files -->
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
